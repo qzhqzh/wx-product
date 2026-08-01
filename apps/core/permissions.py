7872a1743a57da093cde9f54e3ce6558b6015908
qzhqzh <qin28630707@gmail.com>
2026-08-02 00:38:19 +0800

    enforce runtime pipeline constraints

--- apps/core/permissions.py
+++ apps/core/permissions.py
@@ -1,11 +1,16 @@
 from rest_framework.permissions import BasePermission
 
+from .runtime_permissions import enforce_runtime_constraints
+
 
 class HasPipelineRole(BasePermission):
     roles = set()
 
     def has_permission(self, request, view):
-        if request.user and request.user.is_superuser:
+        if not request.user or not request.user.is_authenticated:
+            return False
+        enforce_runtime_constraints(request, view)
+        if request.user.is_superuser:
             return True
         return request.user.groups.filter(name__in=self.roles).exists()
 
